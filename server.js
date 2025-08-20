c1db21a1f5d234979f7b5d24ab1342eaff32c315
Sahil <sahilsingh@gmai.com>
2025-08-20 12:33:35 +0530

    changes done at the same place to create a conflict

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http= require('http');
 
 //data for the receiver
-const data= JSON.stringify({message:"how are you"});
+const data= JSON.stringify({message:"how have you been"});
 
 //creating the option for the request
 const options={
